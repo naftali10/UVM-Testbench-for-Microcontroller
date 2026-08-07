0b4920450afd70868df89fb275b1dcfe6655828d
Ben Fibush <benafibush@gmail.com>
2026-08-07 15:42:52 +0300

    Added function for readability

--- testbench/output_transaction_class.sv
+++ testbench/output_transaction_class.sv
@@ -15,5 +15,13 @@ class output_transaction_class extends uvm_sequence_item;
     `uvm_field_int (dataoutv, UVM_ALL_ON)
     `uvm_field_int (dataout, UVM_ALL_ON)
   `uvm_object_utils_end
+
+
+  function bit is_stalled();
+
+    return stalled;
+
+  endfunction: is_stalled
+
   
 endclass: output_transaction_class
